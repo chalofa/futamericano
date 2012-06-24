36bf81052abc02383a4bfb94117339a1bc737fd6
Chalo Fernandez <chalofa@gmail.com>
2012-06-24 11:46:23 -0500

    Initial load

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Futamericano::Application.config.secret_token = 'a910bd67f930212abef8fe976b0035d7cfc7ed890025be6b77335b5d0e8650e3a893c013e142a8ed0ed74d08f8a0d7d6178f3a7145334c55c6562a1ae373c014'
