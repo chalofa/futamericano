3a640ac91a8321afef0911fdcb91c6e5f378b9e0
Chalo Fernandez <chalofa@gmail.com>
2012-06-24 23:00:26 -0500

    Soon page scaffold

---
+++ app/controllers/soon_controller.rb
@@ -0,0 +1,8 @@
+# Show a counter until season opener
+
+class SoonController < ApplicationController
+
+  def show
+  end
+
+end
---
+++ app/helpers/soon_helper.rb
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------
+# Helper for the Soon counter until season opener
+#-------------------------------------------------------------------------
+
+module SoonHelper
+
+  # TODO: detect season opener
+  def days_to_season_start(start_date = '2012/09/05 8:30 PM ET')
+    seconds = Time.zone.parse(start_date) - Time.zone.now
+    (seconds / 60 / 60 / 24).to_i
+  end
+
+end
