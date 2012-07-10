178a1b5d5f4b6725ab8ad5aa969ba504caf99f3a
Chalo Fernandez <chalofa@gmail.com>
2012-07-10 00:27:03 -0500

    Schema with ActiveAdmin migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,2 @@
 # Create a default user
-# TODO: uncomment after adding ActiveAdmin migration and real data
-#AdminUser.create!(email: 'chalofa@gmail.com', password: '123456', password_confirmation: '123456')
+AdminUser.create!(email: 'chalofa@gmail.com', password: '123456', password_confirmation: '123456')
