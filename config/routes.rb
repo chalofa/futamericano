77bbcd9675daff285bd0a5ce0060c67a8d9b1d60
Chalo Fernandez <chalofa@gmail.com>
2012-07-12 01:18:22 -0500

    Annotated routes (without admin)

--- config/routes.rb
+++ config/routes.rb
@@ -8,3 +8,16 @@ Futamericano::Application.routes.draw do
   root to: 'soon#show'
 
 end
+
+
+
+#== Route Map
+# Generated on 12 Jul 2012 01:17
+#
+#                       soon POST       /soon(.:format)                     soons#create
+#                   new_soon GET        /soon/new(.:format)                 soons#new
+#                  edit_soon GET        /soon/edit(.:format)                soons#edit
+#                            GET        /soon(.:format)                     soons#show
+#                            PUT        /soon(.:format)                     soons#update
+#                            DELETE     /soon(.:format)                     soons#destroy
+#                       root            /                                   soon#show
