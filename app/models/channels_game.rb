343f367e302f67210e8518abe843663307096008
Chalo Fernandez <chalofa@gmail.com>
2012-07-14 14:24:55 -0500

    ChannelGames model

---
+++ app/models/channels_game.rb
@@ -0,0 +1,28 @@
+#-------------------------------------------------------------------------
+#  Intermediate model for updating game's channels
+#-------------------------------------------------------------------------
+
+class ChannelsGame < ActiveRecord::Base
+
+  belongs_to  :game, counter_cache: true
+  belongs_to  :channel
+
+  attr_accessible :game_id, :channel_id
+
+  validates_uniqueness_of :channel_id, scope: :game_id, message: 'duplicated for this Game'
+
+end
+
+
+
+# == Schema Information
+#
+# Table name: channels_games
+#
+#  id         :integer         primary key
+#  game_id    :integer
+#  channel_id :integer
+#  created_at :timestamp
+#  updated_at :timestamp
+#
+
