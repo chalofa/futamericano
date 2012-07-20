5a4bb10557ec9ca88862c31eaa844cb4d1218ffe
Chalo Fernandez <chalofa@gmail.com>
2012-07-20 02:08:47 -0500

    Fix broken Soon controller spec

--- spec/controllers/soon_controller_spec.rb
+++ spec/controllers/soon_controller_spec.rb
@@ -2,10 +2,12 @@ require 'spec_helper'
 
 describe SoonController do
   let(:season) { stub(Season) }
+  let(:game) { stub(Game) }
 
   describe 'GET :show' do
     before do
       Season.stub next_pending: season
+      season.stub_chain(:weeks, :current, :games, :first).and_return game # TODO: use a scope!
       get :show
     end
 
@@ -15,6 +17,10 @@ describe SoonController do
 
     it 'should assign the next season' do
       assigns[:season].should == season
+    end
+
+    it 'should assign the next game' do
+      assigns[:game].should == game
     end
   end
 
